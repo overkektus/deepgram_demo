d459201c0a8fda330226d117eb8954100ca84724
Yahor Piskunou <egor.piskunou@gmail.com>
2023-04-04 14:40:23 +0200

    add useRecorder hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useMediaRecorder } from "./useRecorder";
 
 function App() {
+  const [start, stop, clear, isRun, messages] = useMediaRecorder();
+
+  const handleStart = () => {
+    start();
+  };
+
+  const handleStop = () => {
+    stop();
+  };
+
+  const handleClear = () => {
+    clear();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleClear}>Clear</button>
+      <textarea defaultValue={messages.join(" ")}></textarea>
     </div>
   );
 }
---
+++ src/useRecorder.ts
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+export const useMediaRecorder = () => {
+  const [isRun, setIsRun] = useState(false);
+  const [mediaStream, setMediaStream] = useState<MediaStream | null>(null);
+  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(
+    null
+  );
+  const [websocket, setWebsocket] = useState<WebSocket | null>(null);
+  const [messages, setMessages] = useState<string[]>([]);
+
+  const start = async () => {
+    setIsRun(true);
+    const wsc = new WebSocket("wss://localhost:5002");
+    setWebsocket(wsc);
+
+    wsc.addEventListener("message", (event) => {
+      setMessages((prevMessages) => [...prevMessages, event.data]);
+    });
+
+    wsc.addEventListener("close", () => {
+      stop();
+    });
+
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: false,
+      audio: true,
+    });
+    setMediaStream(stream);
+
+    const recorder = new MediaRecorder(stream, {
+      mimeType: "audio/webm",
+    });
+    setMediaRecorder(recorder);
+
+    recorder.addEventListener("dataavailable", async (event) => {
+      if (event.data.size > 0) {
+        wsc.send(event.data);
+      }
+    });
+
+    recorder.start(2000);
+  };
+
+  const stop = () => {
+    setIsRun(false);
+    if (websocket) {
+      websocket.close();
+    }
+    if (mediaStream) {
+      mediaStream.getAudioTracks().forEach((track) => {
+        track.stop();
+      });
+    }
+    if (mediaRecorder) {
+      mediaRecorder.stop();
+    }
+  };
+
+  const clear = () => {
+    setMessages([]);
+  };
+
+  return [start, stop, clear, isRun, messages] as const;
+};
